08ebb1b80b9b86770c94b6e50fcfdacdc6183738
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-12-10 23:29:19 +0100

    reiniciando o projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,8 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Footer from "./Components/Footer";
-import Header from "./Components/Header";
-import Home from "./Home";
-import Login from "./Components/Login/Login";
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="login/*" element={<Login />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
