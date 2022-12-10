e4a526ab2365fd3f3880876495fad2262efc7aae
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-12-10 07:25:04 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <button>soma</button>
+    </div>
+  );
+}
+
+export default App;
