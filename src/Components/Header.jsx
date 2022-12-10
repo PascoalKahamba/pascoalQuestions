52100be6ccbabc1620a9b6ddeed267f9c8e7ce8d
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-12-10 15:06:56 +0100

    criei o componente login

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import styles from "./Header.module.css";
 import photo from "../assets/dogs.svg";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <img src={photo} alt="logo-dogs" />
-      <div className={styles.login}>Login | Criar</div>
+      <Link to="/">
+        <img src={photo} alt="logo-dogs" />
+      </Link>
+      <Link to="login">
+        <div className={styles.login}>Login | Criar</div>
+      </Link>
     </header>
   );
 };
