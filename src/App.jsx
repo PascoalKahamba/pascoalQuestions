989b4217372b9c8592d6559c86204d0bf01a4d99
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-12-10 08:24:56 +0100

    primeiros estilos na classe header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import React from "react";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
-      <button>soma</button>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import styles from "./Header/module.css";
+import styles from "./Header.module.css";
+import photo from "../assets/dogs.svg";
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <img src="../assets/dogs.svg" alt="logo-dogs" />
+      <img src={photo} alt="logo-dogs" />
       <div>Login | Criar</div>
     </header>
   );
