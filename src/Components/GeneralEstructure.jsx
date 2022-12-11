a36102eb4924510b03a8dbaf1e67cf7eb43d28fc
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-12-11 05:33:25 +0100

    criei o array question

--- src/Components/GeneralEstructure.jsx
+++ src/Components/GeneralEstructure.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 
+const questions = [
+  {
+    question: "Quem fez este aplicativo? ",
+    reply: "Pascoal Kahamba",
+  },
+];
 const GeneralEstructure = () => {
   return <div className="generalClass">Pascoal Kahamba</div>;
 };
