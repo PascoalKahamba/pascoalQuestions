987f6e07c026fd305aa12da31a09d1d883cb7557
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-12-12 07:04:23 +0100

    criei o objeto questions para mostrar as informações

--- src/Components/GeneralEstructure.jsx
+++ src/Components/GeneralEstructure.jsx
@@ -4,10 +4,22 @@ const questions = [
   {
     question: "Quem fez este aplicativo? ",
     reply: "Pascoal Kahamba",
+    reply: "Francisco Fetapi",
+    reply: "Adriano Ngunga",
+    reply: "Feliciano Ndumbo",
   },
 ];
 const GeneralEstructure = () => {
-  return <div className="generalClass">Pascoal Kahamba</div>;
+  return (
+    <div className="generalClass">
+      {questions.map(({ question, reply }) => (
+        <div key={question}>
+          <h1>{question}</h1>
+          <p>{reply}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default GeneralEstructure;
