dd7651890617b921ce275bfe47f832f4498e6c19
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-12-12 15:02:49 +0100

    criei o estado result para sumir com o formulario

--- src/App.jsx
+++ src/App.jsx
@@ -41,20 +41,25 @@ function App() {
     p4: "",
   });
   const [slides, setSlides] = useState(0);
+  const [result, setResult] = useState(null);
+
   function handleChange({ target }) {
     SetReplys({ ...replys, [target.id]: target.value });
   }
-  function result() {
-    console.log("event");
+  function EndResult() {
+    const currects = questions.filter(({ reply, id }) => replys[id] === reply);
+    setResult(`Você acertou: ${currects.length} de ${questions.length}`);
   }
   function handleClick(event) {
     event.preventDefault();
-    if (slides < questions.length) {
+    if (slides < questions.length - 1) {
       setSlides(slides + 1);
     } else {
-      result();
+      setSlides(slides + 1);
+      EndResult();
     }
   }
+
   return (
     <form className="App">
       {questions.map((question, index) => (
@@ -66,7 +71,11 @@ function App() {
           value={replys[question.id]}
         />
       ))}
-      <button onClick={handleClick}>Proximo</button>
+      {result ? (
+        <p>{result}</p>
+      ) : (
+        <button onClick={handleClick}>Proximo</button>
+      )}
     </form>
   );
 }
