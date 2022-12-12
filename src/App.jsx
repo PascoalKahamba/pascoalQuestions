ed536d2b2af1b853673584d1ee471d9911e0c2d7
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-12-12 08:24:12 +0100

    melhorei o objeto questions na App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
 import React from "react";
 import "./App.css";
-import GeneralEstructure from "./Components/GeneralEstructure";
-
+const questions = [
+  {
+    question: "Quem fez este aplicativo? ",
+    options: [
+      "Pascoal Kahamba",
+      "Francisco Fetapi",
+      "Adriano Ngunga",
+      "Feliciano Ndumbo",
+    ],
+    reply: "Pascoal Kahamba",
+    id: "p1",
+  },
+  {
+    question: "Quantas provincias tem Angola? ",
+    options: [20, 16, 18, 11],
+    reply: 18,
+    id: "p2",
+  },
+  {
+    question: "Qual é o animal favorito do Pascoal? ",
+    options: ["Gato", "Cachorro", "Galinha", "Leão"],
+    reply: "Cachorro",
+    id: "p3",
+  },
+  {
+    question: "Quantas faculdades tem a provincia de Benguela? ",
+    options: [5, 3, 12, 1],
+    reply: 1,
+    id: "p4",
+  },
+];
 function App() {
-  return (
-    <div className="App">
-      <GeneralEstructure />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
